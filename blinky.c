f43491b37b30aa902a3419f2a820a134c96485b5
maehw <matthiaz.weber@gmx.de>
2021-02-26 18:16:13 +0100

    Fixed whitespaces (1 tab = 4 spaces)

--- blinky.c
+++ blinky.c
@@ -10,12 +10,12 @@
 
 int main(void)
 {
-	DDRB |= (1<<DDB7);
+    DDRB |= (1<<DDB7);
     while (1) 
     {
-	    PORTB |= (1<<PORTB7);
-	    _delay_ms(1000);
-	    PORTB &= ~ (1<<PORTB7);
-	    _delay_ms(1000);
+        PORTB |= (1<<PORTB7);
+        _delay_ms(1000);
+        PORTB &= ~ (1<<PORTB7);
+        _delay_ms(1000);
     }
 }
